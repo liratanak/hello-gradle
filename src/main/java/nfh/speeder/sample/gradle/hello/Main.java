fe3c616888050c27bf2222b79dea332c553ed2fe
Liratanak <annliratanak@gmail.com>
2014-08-15 15:08:11 +0700

    Init gradle hello

---
+++ src/main/java/nfh/speeder/sample/gradle/hello/Main.java
@@ -0,0 +1,7 @@
+package nfh.speeder.sample.gradle.hello;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello " + new Word().gradle());
+	}
+}
---
+++ src/main/java/nfh/speeder/sample/gradle/hello/Word.java
@@ -0,0 +1,11 @@
+package nfh.speeder.sample.gradle.hello;
+
+public class Word {
+	public CharSequence hello() {
+		return "hello";
+	}
+
+	public CharSequence gradle() {
+		return "gradle";
+	}
+}
